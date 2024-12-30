9bbe05115388b4f1fe3439883858762cea580c80
IvanKoskov <evanmatthewofficial@gmail.com>
2024-12-30 20:16:34 +0700

    Update clone.c

--- clone.c
+++ clone.c
@@ -1,8 +1,32 @@
 #include "clone.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void cloneCommand(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Please provide the repository URL.\n");
+        return;
+    }
 
-void cloneCommand(void){
+    // Get the repo URL from command-line arguments
+    const char *repoUrl = argv[2]; // assuming the URL is provided as the third argument
 
+    // Ask for the destination path
+    printf("Provide destination path for the project: \n");
+    char destinationPath[1024];
+    scanf("%s", destinationPath);
 
-    printf("foo");
+    // Form the git clone command
+    char command[1024];
+    snprintf(command, sizeof(command), "git clone %s %s", repoUrl, destinationPath);
+
+    // Execute the command
+    int result = system(command);
+
+    if (result == 0) {
+        printf("Repository cloned successfully to: %s\n", destinationPath);
+    } else {
+        fprintf(stderr, "Failed to clone repository.\n");
+    }
 }
